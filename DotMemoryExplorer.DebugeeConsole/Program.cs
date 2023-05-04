77a93f86f3a24d112f40ba3184e9026f5ca3c981
Michal Žůrek <zurmich@volny.cz>
2023-05-04 17:47:02 +0200

    Added console type debugge capable of holding symbol obfuscations.

---
+++ DotMemoryExplorer.DebugeeConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using DotMemoryExplorer.DebugeeConsole;
+
+SecretData? secret = null;
+
+void PrintObjects() {
+	SecretData? current = secret;
+	while (current != null) {
+		Console.WriteLine($"Secret item {current.Text} with number {current.Number}");
+		current = current.Prev;
+	}
+}
+
+void AddObject() {
+	Console.WriteLine("Enter secrete text: ");
+	string text = Console.ReadLine() ?? "";
+	Console.WriteLine("Enter secrete number: ");
+	int num;
+	try {
+		num = int.Parse(Console.ReadLine() ?? "");
+	} catch (Exception) {
+		Console.WriteLine("Unable to parse. Ignoring.");
+		return;
+	}
+
+	secret = new SecretData(text, num, secret);
+}
+
+while (true) {
+	Console.WriteLine("==================");
+	Console.WriteLine("Enter command ((p)rint/(a)dde/(c)lose):");
+
+	char command = Console.ReadKey().KeyChar;
+
+	Console.WriteLine();
+	
+	if (command == 'p') {
+		PrintObjects();
+	} else if (command == 'a') {
+		AddObject();
+	} else if (command == 'c') {
+		return;
+	} else {
+		Console.WriteLine("Unknown command");
+	}
+}
---
+++ DotMemoryExplorer.DebugeeConsole/SecretData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotMemoryExplorer.DebugeeConsole {
+	internal class SecretData {
+
+		public string Text { get; }
+
+		public int Number { get; }
+
+		public SecretData? Prev { get; }
+
+		public SecretData(string text, int number, SecretData? prev) {
+			Text = text;
+			Number = number;
+			this.Prev = prev;
+		}
+	}
+}
